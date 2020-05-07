a6a9d4869a3a9b66ce7c5ccc300033a93d135c4e
Sackidude <42474434+sackidude@users.noreply.github.com>
2020-05-07 09:55:30 +0200

    Changing some things

--- main.py
+++ main.py
@@ -4,21 +4,19 @@ I'm going to add a machine learning ai to this thing
 and see what score it can get later(hopefully): )
 """
 
-import os
-
-import normalgame
-from machinelearning import machine_learning
-
 
 def main():
     """Main function. just decides if you are going to boot normal game or machine learning"""
-    input_text = input("Normal Game or Machine learning: [N/M]")
+    input_text = input("Normal Game or Machine learning[N/M]: ")
 
     if input_text == "M":
+        import os
+        from machinelearning import machine_learning
         local_dir = os.path.dirname(__file__)
         config_path = os.path.join(local_dir, 'config.txt')
         machine_learning(config_path)
     else:
+        import normalgame
         normalgame.normal_game()
 
 
